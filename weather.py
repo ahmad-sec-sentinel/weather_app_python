1ae8794a21d48f951e3dad89d226f0dbb4e6f4ba
ahmad-sec-sentinel <erahmad.saalik@gmail.com>
2025-12-17 11:22:11 +0530

    Fix API key environment variable name

--- weather.py
+++ weather.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 def get_current_weather(city="New Delhi"):
-    request_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("API_key")}&units=metric'
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("API_KEY")}&units=metric'
     weather_data = requests.get(request_url).json()
     return weather_data
 
